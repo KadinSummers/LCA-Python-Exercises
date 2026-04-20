36ca34e5faa1d4fd9e9f5c3a741aead7d1377735
Kadin Summers <kadinsummers123@outlook.com>
2026-04-20 16:34:54 +0200

    Exercise 01 - Introduction and Variables

---
+++ week3_ex01_variables.py
@@ -0,0 +1,17 @@
+name = "Kadin Summers" 
+print(name)
+
+age = 20
+print(age)
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+length = int(input("Enter the length of the rectangle: "))
+length = length + 5
+width = int(input("Enter the width of the rectangle: "))
+area = length * width
+print("The area of the rectangle is " + str(area) + ".")
+
+celcius = float(input("Enter the temperature in Celsius: "))
+fahrenheit = (celcius * 9/5) + 32
+print("The temperature in Fahrenheit is " + str(fahrenheit) + ".")
